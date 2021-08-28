3c5ac8f18dd3a998211ef343e11f189efa255c73
Patrick Essiam <pat.essiam701@gmail.com>
2021-08-28 04:13:45 +0000

    Remove comments

--- src/main/java/com/consultation/MedpharmaApplication.java
+++ src/main/java/com/consultation/MedpharmaApplication.java
@@ -15,11 +15,6 @@ public class MedpharmaApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(MedpharmaApplication.class, args);
-//        ViewConsultations viewConsultations = new ViewConsultations();
-//        System.out.println("all: " + viewConsultations.allConsultations());
-
-//        AddConsultation add = new AddConsultation();
-//        System.out.println("add: " + add.addConsultation(new Consultation(2, "asdf", "wer", "eiei", "jdkd", "iiie")));
     }
 
 }
